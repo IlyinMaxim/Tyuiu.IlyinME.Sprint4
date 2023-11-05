b640989b6230e91d1eef613be29f0f687d5276da
Tyuiu.IlyinME@outlook.com <maxchance05@mail.ru>
2023-11-05 15:31:56 +0500

    Task3

---
+++ Tyuiu.IlyinME.Sprint4.Task3.V9/Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Tyuiu.IlyinME.Sprint4.Task3.V9.Lib;
+
+namespace Tyuiu.IlyinME.Sprint4.Task3.V9
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+
+            Console.Title = "Спринт #4 | Выполнил: Ильин М. Е. | АСОиУБ-23-1";
+
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* Спринт #4                                                               *");
+            Console.WriteLine("* Тема: Двумерные массивы. (статический ввод)                             *");
+            Console.WriteLine("* Задание #3                                                              *");
+            Console.WriteLine("* Вариант #9                                                              *");
+            Console.WriteLine("* Выполнил: Ильин Максим Евгеньевич | АСОиУБ-23-1                         *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                                *");
+            Console.WriteLine("* Дан двумерный целочисленный массив 5 на 5 элементов,                    *");
+            Console.WriteLine("* заполненный статическими значениями в диапазоне от 3 до 9.              *");
+            Console.WriteLine("* Найдите сумму элементов во втором столбце массива.                      *");
+            Console.WriteLine("*                                                                         *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+            Console.WriteLine("***************************************************************************");
+            int[,] array = new int[5, 5] { { 9, 6, 4, 5, 3 },
+                                           { 7, 4, 7, 5, 3 },
+                                           { 8, 5, 9, 9, 3 },
+                                           { 7, 8, 7, 9, 3 },
+                                           { 3, 7, 3, 7, 7 } };
+            int rows = array.GetUpperBound(0) + 1;
+            int columns = array.Length / rows;
+
+            Console.WriteLine("Массив: ");
+            for (int i = 0; i < rows; i++)
+            {
+                for (int j = 0; j < columns; j++)
+                {
+                    Console.Write($"{array[i, j]} \t");
+                }
+
+                Console.WriteLine();
+
+            }
+            
+            Console.WriteLine();
+
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("Сумма элементов во втором столбце массива = " + ds.Calculate(array));
+
+            Console.ReadKey();
+        }
+    }
+}
