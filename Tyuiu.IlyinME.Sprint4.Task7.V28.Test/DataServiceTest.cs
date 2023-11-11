a8b019af7993e5d6cca01943376dae817204c9aa
Tyuiu.IlyinME@outlook.com <maxchance05@mail.ru>
2023-11-11 22:22:54 +0500

    Task7

---
+++ Tyuiu.IlyinME.Sprint4.Task7.V28.Test/DataServiceTest.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+using Tyuiu.IlyinME.Sprint4.Task7.V28.Lib;
+
+namespace Tyuiu.IlyinME.Sprint4.Task7.V28.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void ValidCalculate()
+        {
+            int rows = 5;
+            int columns = 3;
+            int[,] matrix = new int[rows, columns];
+            string str = "623351179845632";
+            DataService ds = new DataService();
+            int res = ds.Calculate(rows, columns, str);
+            int wait = 4608;
+            Assert.AreEqual(wait, res);
+        }
+    }
+}
---
+++ Tyuiu.IlyinME.Sprint4.Task7.V28.Test/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Tyuiu.IlyinME.Sprint4.Task7.V28.Test")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Tyuiu.IlyinME.Sprint4.Task7.V28.Test")]
+[assembly: AssemblyCopyright("Copyright ©  2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("2f3196f9-0672-4808-b68e-a5113ce95d96")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
